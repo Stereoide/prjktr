8128b77c0ad5014bca6cb570a3c3977c7ff803c8
Jörn Maske <j.maske@otterbach.de>
2018-01-18 14:56:19 +0100

    Added begin timestamps to find-unfinished-worklogs mails

--- resources/views/emails/unfinished-worklogs-text.blade.php
+++ resources/views/emails/unfinished-worklogs-text.blade.php
@@ -2,4 +2,5 @@ Die folgenden Arbeiten sind zum aktuellen Zeitpunkt noch nicht abgeschlossen:
 @foreach ($worklogs as $worklog)
 
 {{ $worklog->job->project->name }} / {{ $worklog->job->subproject->name }} @if (!empty($worklog->notes))/ {{ $worklog->notes }} @endif
+(seit {{ $worklog->begin_at->format('d.m.Y H:i') }} Uhr)
 @endforeach
--- resources/views/emails/unfinished-worklogs.blade.php
+++ resources/views/emails/unfinished-worklogs.blade.php
@@ -11,6 +11,7 @@
 
 @foreach ($worklogs as $worklog)
     {{ $worklog->job->project->name }} / {{ $worklog->job->subproject->name }} @if (!empty($worklog->notes)) / {{ $worklog->notes }} @endif <br />
+    (seit {{ $worklog->begin_at->format('d.m.Y H:i') }} Uhr)
 @endforeach
 </body>
 </html>

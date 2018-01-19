3d4a6f8a359a29f12974853797f5c47d95000f31
Jörn Maske <j.maske@otterbach.de>
2018-01-19 09:22:56 +0100

    Fixed insecure asset inclusion

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,7 +13,7 @@
 
 	<!-- Styles -->
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-	<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script>
